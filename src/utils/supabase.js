ec613ae5494f45fc99d71866ae60418049d3042b
Yanela-1 <ryeyanela30@gmail.com>
2026-05-27 06:09:12 +0800

    hubad

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const SUPABASE_URL = 'https://pjkkoedltytgvjunuwyc.supabase.co';
-const SUPABASE_SERVICE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBqa2tvZWRsdHl0Z3ZqdW51d3ljIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3OTgxNjM3NiwiZXhwIjoyMDk1MzkyMzc2fQ.w10k3VeBwgRwQJ3tEchUhfZwcRZBAfZthJ2IF6Ql-cU'; // ← paste your service role key here
+const SUPABASE_URL = process.env.EXPO_PUBLIC_SUPABASE_URL;
+const SUPABASE_SERVICE_KEY = process.env.EXPO_PUBLIC_SUPABASE_SERVICE_KEY;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_KEY, {
   auth: {
@@ -12,4 +12,3 @@ export const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_KEY, {
     detectSessionInUrl: false,
   },
 });
- 
